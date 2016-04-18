3cf03a7197c1ca84bd6916fc3be5918f692c7eb0
alxest <youngju.song@sf.snu.ac.kr>
2016-04-18 21:15:55 +0900

    Update gitignore

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,15 @@
+import scala.io.Source
 
+class graph {
+  val size: Int
+  val edges: Set[(Int, Int, Int)]
+}
 
 object main extends Application {
-  
+  val n_cand : Set[Int] = Set(100, 500, 1000)
+  val m_cand : Set[Int] = Set(0, 10, 100, 1000, 10000)
+  for(i <- n_cand ;
+    j <- m_cand
+  ) {
+  }
 }
