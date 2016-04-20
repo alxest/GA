d13ce2287de775e06998a53d298d6997d19ae163
alxest <youngju.song@sf.snu.ac.kr>
2016-04-20 23:40:38 +0900

    Working...

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ organization := "generator"
 name := "default"
 
 version := "0.1-SNAPSHOT"
+
+libraryDependencies += "com.github.pathikrit" %% "better-files" % "2.13.0"
+// http://mvnrepository.com/artifact/com.github.pathikrit/better-files_2.11/2.13.0
+
+val buildSettings = Defaults.defaultSettings ++ Seq(
+   javaOptions += "-Xmx4G")
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,15 +1,184 @@
 import scala.io.Source
-
-class graph {
-  val size: Int
-  val edges: Set[(Int, Int, Int)]
+import better.files._, Cmds._
+import java.io.{File => JFile}
+import scala.annotation.tailrec
+import scala.util.Random
+// http://stackoverflow.com/questions/15639078/scala-class-constructor-parameters
+// If you prefix parameters with val, var they will be visible from outside of class, otherwise, they will be private, as you can see in code above.
+class Edge(val from: Int, val to: Int, val weight: Int) {
+  override def toString =
+    from.toString + " " + to.toString + " " + weight.toString
+  // where did this "weight" come from??
+  override def equals(o: Any) = o match {
+    case (that: Edge) =>
+      this.from == that.from && this.to == that.to
+      // val r = this.from == that.from && this.to == that.to
+      // println(r)
+      // r
+    case _ => false
+  }
+  override def hashCode = (from + to).hashCode
 }
 
-object main extends Application {
-  val n_cand : Set[Int] = Set(100, 500, 1000)
-  val m_cand : Set[Int] = Set(0, 10, 100, 1000, 10000)
-  for(i <- n_cand ;
-    j <- m_cand
-  ) {
+class Graph(val size: Int, val edges: Set[Edge]) {
+  def this(size: Int) = this(size, Set())
+  override def toString = edges.foldLeft("")(
+    (s: String, x: Edge) => x.toString + "\n" + s)
+  def random_node = Random.nextInt(size) + 1
+  def spread_edges(m: Int, forbidden: Set[Range]) = {
+    // var counter = 0
+    // @tailrec
+    def stream: Stream[Edge] = {
+      // println(counter)
+      // counter += 1
+      (new Edge(random_node, random_node, Graph.random_weight) #:: stream)
+      // (new Edge(random_node, random_node, Graph.random_weight) #:: stream).distinct
+    }
+    // println("-----------------------------------------------------------")
+    // println(stream.take(10000))
+    // println("-----------------------------------------------------------")
+    // println(stream.take(100).toList)
+    // println("-----------------------------------------------------------")
+    // println(stream.take(1000).toList)
+    // println("-----------------------------------------------------------")
+    // println(stream.take(1000).toList)
+    // println("-----------------------------------------------------------")
+
+    val bb: Set[Edge] = stream.distinct.take(m).filter(x =>
+    forbidden.forall(! _.contains(x))).toSet.take(m)
+    // println(bb)
+    val e = edges ++ bb
+    // println(e)
+    //     override def distinct: Stream[A] = {
+  //   // This should use max memory proportional to N, whereas
+  //   // recursively calling distinct on the tail is N^2.
+  //   def loop(seen: Set[A], rest: Stream[A]): Stream[A] = {
+  //     if (rest.isEmpty) rest
+  //     else if (seen(rest.head)) loop(seen, rest.tail)
+  //     else cons(rest.head, loop(seen + rest.head, rest.tail))
+  //   }
+  //   loop(Set(), this)
+  // }
+
+    
+      // (1 to m).map(_ =>
+      //   new Edge(random_node, random_node, Graph.random_weight))
+    new Graph(size, e)
   }
+
+  // implicit override def toString = ""
+  // override def abc = ""
+  // method abc overrides nothing
+}
+
+object Graph {
+  def random_weight = Random.nextInt(2000) - 1000
+  // import scala.language.implicitConversions
+  // implicit def GraphToString(g: Graph): String = ""
+}
+
+object DataGenerator extends Application {
+  val n_cand : Set[Int] = Set(100, 500, 1000, 2500)
+  val m_cand : Set[Int] = Set(0, 10, 100, 1000, 10000, 25000)
+  for(n <- n_cand ;
+    m <- m_cand
+  ) {
+    if(n*n >= m) {
+      println(s"${n} ${m}")
+      def write_graph(file_name: String, g: Graph) = {
+        val f = File(file_name)
+        // f.delete(true)
+        f < ""
+        f << s"${n} ${m}"
+        f << g.toString
+      }
+
+      write_graph(s"${n}_${m}_normal.in", new Graph(n).spread_edges(m, Set()))
+      // write_graph(s"${n}_${m}_clique.in", new Graph(n).spread_edges(m, Set()))
+      // write_graph(s"${n}_${m}_2clique.in", new Graph(n).spread_edges(m, Set()))
+      // write_graph(s"${n}_${m}_coclique.in", new Graph(n).spread_edges(m, Set()))
+      // write_graph(s"${n}_${m}_2coclique.in", new Graph(n).spread_edges(m, Set()))
+      // val f_clique1 = File(s"${n}_${m}_clique1.in")
+      // f < ""
+      // f << s"${n}"
+      // f << s"${m}"
+      // val g = new Graph(n, Set()).spread_edges(m, Set())
+      // f << g.toString
+
+    }
+  }
+}
+
+class GA[A](
+  val pool: List[A],
+  val crossover: (A, A) => A,
+  val mutation: A => A,
+  val valuation: A => Float,
+  val selection: List[Float] => List[Float]) {
+  val progress: GA[A] = {
+    val next_pool: List[A] = pool.map(x => (x, valuation(x))).map(x => x._1)
+    new GA[A](next_pool, crossover, mutation, valuation, selection)
+  }
+}
+
+object GA {
+  val pool_size = 250
+}
+
+class ListIntChromosome(length: Int, maxval: Int) { // extends Chromosome {
+  type A = List[Int]
+  def random_binary: A = {
+    // val t: IndexedSeq[String] = (0 to n).map(_ =>
+    //   (scala.util.Random.nextInt % 2).toString)
+    // t.foldLeft("")(_ + _)
+    (0 to length).map(_ => Random.nextInt(maxval)).toList
+  }
+  def crossover(a: A, b: A): A = {
+    assert(a.size == b.size)
+    assert(b.size == length)
+    val l = a.size
+    val k = Random.nextInt(l)
+    a.slice(0, k) ++ b.slice(k, l)
+  }
+  def mutation(a: A): A = {
+    assert(a.size == length)
+    val k = Random.nextInt(a.size)
+    a.updated(k, Random.nextInt(maxval))
+  }
+}
+
+object SelectionFunction {
+  def basic(a: List[Float]): List[Float] = ???
+}
+
+object GARunner extends Application {
+  val dir = File(System.getProperty("user.dir"))
+  val matches: Iterator[File] = dir.glob("**/*.{in}")
+  matches.foreach { f =>
+    println(f)
+    val lines: Array[String] = f.lines.toArray
+    val (n, m) = {
+      val t = lines(0).split(" ")
+      (t(0).toInt, t(1).toInt)
+    }
+    val edge_data = lines.drop(1).filterNot(_.trim == "").map(_.split(" ")).map(
+      x => {
+        assert(x.length == 3, s"${x} length not 3, but ${x.length}")
+        new Edge(x(0).toInt, x(1).toInt, x(2).toInt)
+      }
+    ).toSet
+    assert(edge_data.size == m)
+    val g = new Graph(n, edge_data)
+    val LIC = new ListIntChromosome(n, 2)
+    val ga = new GA[List[Int]](
+      List.fill(GA.pool_size)(LIC.random_binary),
+      LIC.crossover,
+      LIC.mutation,
+      ???,
+      SelectionFunction.basic
+    )
+    println(s"${n} ${m}")
+  }
+  // val dir: better.files.File = pwd / cwd
+  // val matches: Iterator[File] = dir.glob("**/*.{java,scala}")
 }
